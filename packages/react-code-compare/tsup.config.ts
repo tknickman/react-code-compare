a416a6e8f99d81f21b89947efe07fef222e1e08e
tknickman <tom.knickman@vercel.com>
2023-03-29 16:36:44 -0400

    maybe minify

--- packages/react-code-compare/tsup.config.ts
+++ packages/react-code-compare/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig((options: Options) => ({
   entry: ["src/**/*.ts", "src/**/*.tsx"],
   format: ["cjs"],
   dts: true,
-  minify: false,
+  minify: true,
   clean: true,
   external: ["react"],
   ...options,
